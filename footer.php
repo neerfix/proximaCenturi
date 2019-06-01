19cf439926320318eb7194fb5d394fa266554f83
Neerfix <nicolas.notararigo@ynov.com>
2019-06-01 22:08:35 +0200

    Update version and style.

--- footer.php
+++ footer.php
@@ -1,5 +1,15 @@
 <footer class="site__footer">
     <?php wp_nav_menu( array( 'theme_location' => 'footer' ) ); ?>
+    <a href="<?php get_home_url(); ?>" class="logo">
+        <img src="http://notararigo-nicolas.local/wp-content/uploads/2019/05/logo.png" alt="" class="logo">
+    </a>
+    <div class="footer-menu--right">
+        <ul class="menu">
+            <li><a href="https://www.linkedin.com/in/nicolas-notararigo/" target="_blank"><i class="fab fa-linkedin"></i></a></li>
+            <li><a href="https://github.com/Neerfix" target="_blank"><i class="fab fa-github"></i></a></li>
+        </ul>
+        <p>&copy; <?php date('Y'); ?> Copyright. Tous droits réservés.</p>
+    </div>
 </footer>
 
 <?php wp_footer(); ?>
--- header.php
+++ header.php
@@ -4,12 +4,20 @@
     <meta charset="<?php bloginfo('charset'); ?>">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no"/>
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <script src="https://kit.fontawesome.com/cecc6a8642.js"></script>
     <?php wp_head(); ?>
 </head>
 
 <body <?php body_class( 'site' ); ?>>
-
-
-<header class="site__header">
-    
+<header class="header">
+    <div class="header-left" wfd-id="14">
+        <a href="https://www.nicolasnotararigo.com/" class="logo">
+            <img src="https://www.nicolasnotararigo.com/assets/img/logo.png" alt="Nicolas Notararigo - logo" title="logo" class="logo">
+        </a>
+    </div>
+    <div class="header-right" wfd-id="13">
+        <a href="https://www.nicolasnotararigo.com/contact_me" class="btn btn-red">
+            Me contacter
+        </a>
+    </div>
 </header>
