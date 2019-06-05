fa7f42a7e615ebef87bcb133a834211fe0780f75
Neerfix <nicolas.notararigo@ynov.com>
2019-06-05 17:14:59 +0200

    change page for delete h1.Le blog du capitaine, delete link to read more ...

--- page.php
+++ page.php
@@ -1,17 +1,10 @@
 <?php get_header(); ?>
-    <h1>Le blog Capitaine WP</h1>
-
 <?php if( have_posts() ) : while( have_posts() ) : the_post(); ?>
 
-<div class="container text-white">
+<div class="container text-white text-center">
     <article class="post">
-        <h2><?php the_title(); ?></h2>
-
+        <h1><?php the_title(); ?></h1>
         <p><?php the_content(); ?></p>
-
-        <p>
-            <a href="<?php the_permalink(); ?>" class="post__link">Lire la suite</a>
-        </p>
     </article>
 </div>
 <?php endwhile; endif; ?>
