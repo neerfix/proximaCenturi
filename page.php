eb40555696e7999f22253495dc79c1b7675cb10a
Neerfix <nicolas.notararigo@ynov.com>
2019-06-05 17:26:47 +0200

    fix error

--- page.php
+++ page.php
@@ -1,7 +1,7 @@
 <?php get_header(); ?>
 <?php if( have_posts() ) : while( have_posts() ) : the_post(); ?>
 
-<div class="container text-white text-center">
+<div class="container text-center">
     <article class="post">
         <h1><?php the_title(); ?></h1>
         <p><?php the_content(); ?></p>
